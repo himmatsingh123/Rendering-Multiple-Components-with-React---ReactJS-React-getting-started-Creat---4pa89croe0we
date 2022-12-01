bebe7478bb938efc6d724367d9496d8e6f49ad73
himmatsingh123 <103628614+himmatsingh123@users.noreply.github.com>
2022-12-01 22:55:22 +0530

    Update App.js
    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,15 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+     <h1 data-ns-test = "project-name"> React App </h1>
+      <p  data-ns-test = "project-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Maecenas pulvinar nisl et mi feugiat gravida. Proin elementum a lectus a faucibus. Nullam in mattis tortor. 
+    In urna felis, pellentesque vel luctus sit amet, ultrices et ligula. Sed in odio a tortor ullamcorper porta. 
+    Aenean vel eleifend velit. Nulla nec enim euismod, ornare erat in, iaculis urna. Suspendisse accumsan lorem at ex varius, at lacinia mi semper. 
+    Ut vitae tincidunt leo. Donec mattis ex sit amet tortor egestas, in mattis odio luctus. Proin vel dictum nisi. Suspendisse sed porttitor ante. 
+    Mauris egestas justo a lectus blandit tincidunt. Praesent id euismod quam. Curabitur maximus scelerisque lectus. </p>
+    </div>
   )
 }
 
